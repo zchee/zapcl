4259163c5b336f1819ac98b09f108f0f577ba2cc
Koichi Shiraishi <zchee.io@gmail.com>
2022-08-17 05:31:15 +0900

    zapcloudlogging: add ServiceContext testcase
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ service_test.go
@@ -0,0 +1,28 @@
+// Copyright 2022 The zap-cloudlogging Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package zapcloudlogging
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestServiceContext(t *testing.T) {
+	t.Parallel()
+
+	field := zapcore.Field{
+		Key:  "serviceContext",
+		Type: zapcore.ObjectMarshalerType,
+	}
+
+	if diff := cmp.Diff(field, ServiceContext("test service name"),
+		cmpopts.IgnoreFields(zap.Field{}, "Interface"),
+	); diff != "" {
+		t.Fatalf("(-want, +got)\n%s\n", diff)
+	}
+}
