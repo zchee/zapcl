9598b6dbd302f8c621001cf44f34c80eb0ea30cb
Koichi Shiraishi <zchee.io@gmail.com>
2022-09-11 04:29:42 +0900

    all: fix module name
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- source_location_test.go
+++ source_location_test.go
@@ -13,7 +13,7 @@ func TestSourceLocation(t *testing.T) {
 
 	got := SourceLocation(Caller(0)).Interface.(*sourceLocation)
 
-	if gotFile, wantFile := got.File, "zap-cloudlogging/source_location_test.go"; !strings.Contains(gotFile, wantFile) {
+	if gotFile, wantFile := got.File, "zapcl/source_location_test.go"; !strings.Contains(gotFile, wantFile) {
 		t.Errorf("except contains got %s in %s", gotFile, wantFile)
 	}
 	if gotLine, wantLine := got.Line, int64(14); gotLine != wantLine {
@@ -29,7 +29,7 @@ func TestNewSource(t *testing.T) {
 
 	got := newSource(Caller(0))
 
-	if gotFile, wantFile := got.File, "zap-cloudlogging/source_location_test.go"; !strings.Contains(gotFile, wantFile) {
+	if gotFile, wantFile := got.File, "zapcl/source_location_test.go"; !strings.Contains(gotFile, wantFile) {
 		t.Fatalf("except contains got %s in %s", gotFile, wantFile)
 	}
 	if gotLine, wantLine := got.Line, int64(30); gotLine != wantLine {
