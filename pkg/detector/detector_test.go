15270f02b6e2a809f16788f3f9ae8e6d4aca9e4c
Koichi Shiraishi <zchee.io@gmail.com>
2022-09-22 07:19:06 +0900

    pkg/detector: add initial testcase
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ pkg/detector/detector_test.go
@@ -0,0 +1,82 @@
+// Copyright 2022 The zap-cloudlogging Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package detector
+
+import (
+	"testing"
+)
+
+func TestCloudPlatformAppEngineStandard(t *testing.T) {
+	d := NewDetector(&fakeResourceGetter{
+		envVars: map[string]string{
+			EnvAppEngineFlexService: "foo",
+			EnvAppEngineEnv:         "standard",
+		},
+	})
+	platform := d.CloudPlatform()
+
+	if platform != AppEngineStandard {
+		t.Fatalf("got %d but want %d", platform, AppEngineStandard)
+	}
+}
+
+func TestCloudPlatformAppEngineFlex(t *testing.T) {
+	d := NewDetector(&fakeResourceGetter{
+		envVars: map[string]string{
+			EnvAppEngineFlexService:  "foo",
+			EnvAppEngineFlexVersion:  "001",
+			EnvAppEngineFlexInstance: "foo",
+		},
+	})
+	platform := d.CloudPlatform()
+
+	if platform != AppEngineFlex {
+		t.Fatalf("got %d but want %d", platform, AppEngineFlex)
+	}
+}
+
+func TestCloudPlatformCloudRun(t *testing.T) {
+	d := NewDetector(&fakeResourceGetter{
+		envVars: map[string]string{
+			EnvCloudRunService:  "foo",
+			EnvCloudRunRevision: "foo-001",
+			EnvCloudRunConfig:   "foo",
+		},
+	})
+	platform := d.CloudPlatform()
+
+	if platform != CloudRun {
+		t.Fatalf("got %d but want %d", platform, CloudRun)
+	}
+}
+
+func TestCloudPlatformCloudRunJobs(t *testing.T) {
+	d := NewDetector(&fakeResourceGetter{
+		envVars: map[string]string{
+			EnvCloudRunJobsService:  "foo",
+			EnvCloudRunJobsRevision: "foo-001",
+		},
+	})
+	platform := d.CloudPlatform()
+
+	if platform != CloudRunJobs {
+		t.Fatalf("got %d but want %d", platform, CloudRunJobs)
+	}
+}
+
+func TestCloudPlatformCloudFunctions(t *testing.T) {
+	d := NewDetector(&fakeResourceGetter{
+		envVars: map[string]string{
+			EnvCloudFunctionsTarget:        "foo",
+			EnvCloudFunctionsSignatureType: "foo",
+			EnvCloudFunctionsKService:      "foo",
+			EnvCloudFunctionsKRevision:     "foo",
+		},
+	})
+	platform := d.CloudPlatform()
+
+	if platform != CloudFunctions {
+		t.Fatalf("got %d but want %d", platform, CloudFunctions)
+	}
+}
---
+++ pkg/detector/fake_test.go
@@ -0,0 +1,38 @@
+// Copyright 2022 The zap-cloudlogging Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package detector
+
+// fakeResourceGetter mocks internal.ResourceAtttributesGetter interface to retrieve env vars and metadata
+type fakeResourceGetter struct {
+	envVars  map[string]string
+	metaVars map[string]string
+	fsPaths  map[string]string
+}
+
+func (g *fakeResourceGetter) EnvVar(name string) string {
+	if g.envVars != nil {
+		if v, ok := g.envVars[name]; ok {
+			return v
+		}
+	}
+	return ""
+}
+
+func (g *fakeResourceGetter) Metadata(path string) string {
+	if g.metaVars != nil {
+		if v, ok := g.metaVars[path]; ok {
+			return v
+		}
+	}
+	return ""
+}
+
+func (g *fakeResourceGetter) ReadAll(path string) string {
+	if g.fsPaths != nil {
+		if v, ok := g.fsPaths[path]; ok {
+			return v
+		}
+	}
+	return ""
+}
