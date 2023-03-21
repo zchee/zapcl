90544dbcb7a1290b363ef0537043aefe26442047
Koichi Shiraishi <zchee.io@gmail.com>
2023-03-22 01:07:42 +0900

    zapcl: split package synopsis
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- zapcl.go
+++ zapcl.go
@@ -1,7 +1,6 @@
 // Copyright 2022 The zapcl Authors
 // SPDX-License-Identifier: BSD-3-Clause
 
-// Package zapcloudlogging provides the Cloud Logging integration for Zap.
 package zapcl
 
 import (
